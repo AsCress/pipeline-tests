80a509179c9c5bcdd907e711ff0c680c85eb9cef
Anashuman Singh <anash.cheema@gmail.com>
2024-07-16 20:01:56 +0530

    chore: test

--- app/build/generated/ap_generated_sources/debug/out/io/realm/io_pslab_models_SensorDataBlockRealmProxyInterface.java
+++
@@ -1,9 +0,0 @@
-package io.realm;
-
-
-public interface io_pslab_models_SensorDataBlockRealmProxyInterface {
-    public long realmGet$block();
-    public void realmSet$block(long value);
-    public String realmGet$sensorType();
-    public void realmSet$sensorType(String value);
-}
